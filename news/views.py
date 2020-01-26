8d00033baa8d5ef619b5f5317f9e37c066869167
Kenneth-joseph <josephochieng812@gmail.com>
2020-01-27 01:00:40 +0300

    added a function in the news/view file that helps us find the right day of the week

--- news/views.py
+++ news/views.py
@@ -1,8 +1,31 @@
 from django.http import HttpResponse
+import datetime as dt
 from django.shortcuts import render
 
 
 # Create your views here.
 def welcome(request):
-
     return HttpResponse('Welcome to Moringa School')
+
+
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+        <body>
+        <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+        '''
+    return HttpResponse(html)
+
+
+def convert_dates(dates):
+    # function that gets the week day number of date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', "Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
