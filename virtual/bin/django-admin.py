3a8bac411a9857487454bb69dddbfd955f271296
Kenneth-joseph <josephochieng812@gmail.com>
2020-01-30 16:00:31 +0300

    fixed my gitignore file

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/moringa/Desktop/Django/The-Moringa-Tribune/virtual/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
